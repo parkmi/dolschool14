e7877e6c1bc5393c97b51254ed328a7d899814f3
oldvasya <oldvasya@gmail.com>
2012-01-16 10:11:01 +0000

    Удалены лишние шаблоны

--- templates/city_portal/js/s5_clr_fix.js
+++
@@ -1,7 +0,0 @@
-var s5_div_clr =
-document.getElementById("s5_maincolumn").getElementsByTagName("DIV");
-        for (var s5_div_clra=0; s5_div_clra<s5_div_clr.length; s5_div_clra++) {
-        if (s5_div_clr[s5_div_clra].className) {
-        s5_div_clr[s5_div_clra].className = "";
-        }
-        }
