f78a3492b605906dbd0f5aefb39c22072c4450d6
oldvasya@gmail.com <oldvasya@gmail.com@f0f167ad-9f3d-44b0-0976-54913a7e4252>
2012-01-16 10:11:01 +0000

    Удалены лишние шаблоны

--- trunk/templates/city_portal/js/s5_s5box.js
+++
@@ -1,6 +0,0 @@
-	var s5_fa_box = document.getElementById("s5_box_sm_inner").getElementsByTagName("H3");
-	for (var fa_box=0; fa_box<s5_fa_box.length; fa_box++) {
-		document.getElementById("s5_login").innerHTML = s5_fa_box[fa_box].innerHTML;
-		s5_fa_box[fa_box].style.display = "none";
-		break;
-	}
